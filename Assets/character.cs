df310f8d283ca634a4f4c6c9b95dcb9ad7b27f88
Andrew Grusby <ajg521@nyu.edu>
2014-02-10 12:40:28 -0500

    some edits to platformer

--- Assets/character.cs
+++ Assets/character.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class character : MonoBehaviour {
 
-	public float speed = 3f;
-	public float jumpSpeed = 10f;
-	public float gravity = 20f;
+	private float speed = 3f;
+	private float jumpSpeed = 50f;
+	private float gravity = 8f;
+	private Vector3 moveDirection = Vector3.zero;
+
 
 	//function for rotating
 	public void Rotating ( float Degrees ) {
@@ -16,22 +18,20 @@ public class character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//momentum is set to 0 every frame
-		Vector3 moveDirection = Vector3.zero;
-
 		CharacterController controller = GetComponent<CharacterController>();
 
 		if (controller.isGrounded) {
 			//spacebar jumps up
 			if (Input.GetKey(KeyCode.Space))
-				moveDirection += transform.up*jumpSpeed;
+				moveDirection.y = jumpSpeed;
 				}
 
 		//moves forward/back if W/S are pressed
 		if (Input.GetKey(KeyCode.W))
-			moveDirection = transform.forward*speed;
+			moveDirection += transform.forward*speed;
 
 		if (Input.GetKey(KeyCode.S))
-			moveDirection = -transform.forward*speed;
+			moveDirection += -transform.forward*speed;
 		//rotates if A/D are pressed
 		if (Input.GetKey (KeyCode.A)) {
 			Rotating (5f);
@@ -43,5 +43,7 @@ public class character : MonoBehaviour {
 		moveDirection.y -= gravity;
 
 		controller.Move(moveDirection * Time.deltaTime);
+		if (controller.isGrounded) 
+			moveDirection = Vector3.zero;
 	}
 }
